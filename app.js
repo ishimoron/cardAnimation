41468f2c5e170c14fc71063f2649a6e398f1f2a6
ishimoron <super.evgeni423@gmail.com>
2021-09-12 17:24:57 +0300

    add activeSlider function

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const slides = document.querySelectorAll('.slide')
+function cardPlugin(number) {
+    const slides = document.querySelectorAll('.slide')
 
-for(const slide of slides) {
-    slide.addEventListener('click', () => {
-        clearSlides()
-        slide.classList.add('active')
-    })
+    slides[number].classList.add('active')
+
+    for(const slide of slides) {
+        slide.addEventListener('click', () => {
+            clearSlides()
+            slide.classList.add('active')
+        })
+    }
+    
+    function clearSlides(){
+        slides.forEach(slide => {
+            slide.classList.remove('active')
+        });
+    
+    }
 }
 
-function clearSlides(){
-    slides.forEach(slide => {
-        slide.classList.remove('active')
-    });
-
-}
-
+cardPlugin(0)
